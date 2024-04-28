77e25ebfcff593a3018335ce1ecff82bd25ff9cd
jake <jayteedubya@gmail.com>
2024-04-28 16:47:14 -0700

    added embedded hal for led lighting

--- bbc_micro_bit/src/main.rs
+++ bbc_micro_bit/src/main.rs
@@ -1,19 +1,24 @@
-#![no_std]
 #![no_main]
-
-
+#![no_std]
 
 use cortex_m::asm::nop;
+use rtt_target::{rprintln, rtt_init_print};
 use cortex_m_rt::entry;
 //use panic_halt as _;
-use rtt_target::{rprintln, rtt_init_print};
-use microbit;
+use microbit::board::Board;
+use microbit::hal::prelude::*;
+use embedded_hal::digital::OutputPin;
 
 
 #[entry]
 unsafe fn main() -> ! {
     rtt_init_print!();
     rprintln!("Howdy, I am micro bit");
+    
+    let mut board = Board::take().unwrap();
+
+    board.display_pins.col1.set_high().unwrap();
+    board.display_pins.row1.set_high().unwrap();
     loop {
         rprintln!("I'm OK");
         for _ in 0..10000 {
