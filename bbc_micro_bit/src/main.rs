6fc814fd60cd408b4ccd0c40014a4c4a4528f96f
jake <jayteedubya@gmail.com>
2024-04-28 17:06:40 -0700

    light up a strip of leds

--- bbc_micro_bit/src/main.rs
+++ bbc_micro_bit/src/main.rs
@@ -6,24 +6,37 @@ use rtt_target::{rprintln, rtt_init_print};
 use cortex_m_rt::entry;
 //use panic_halt as _;
 use microbit::board::Board;
+use microbit::hal::timer::Timer;
 use microbit::hal::prelude::*;
 use embedded_hal::digital::OutputPin;
+use embedded_hal::delay::DelayNs;
 
 
 #[entry]
 unsafe fn main() -> ! {
     rtt_init_print!();
-    rprintln!("Howdy, I am micro bit");
-    
     let mut board = Board::take().unwrap();
 
-    board.display_pins.col1.set_high().unwrap();
-    board.display_pins.row1.set_high().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+
+    // set the columns low to select them, then select the leds to light up with rows.
+    board.display_pins.col1.set_low().unwrap();
+    board.display_pins.col2.set_low().unwrap();
+    board.display_pins.col3.set_low().unwrap();
+    board.display_pins.col4.set_low().unwrap();
+    // put the rows in vars, otherwise it doesn;t work. not sure why yet.
+    let mut row1 = board.display_pins.row1;
+    let mut row2 = board.display_pins.row2;
+
     loop {
-        rprintln!("I'm OK");
-        for _ in 0..10000 {
-            nop(); // no op instruction to add delay
-        }
+        row1.set_low().unwrap();
+        row2.set_low().unwrap();
+        rprintln!("Dark!");
+        timer.delay_ms(1_000_u32);
+        row1.set_high().unwrap();
+        row2.set_high().unwrap();
+        rprintln!("Light!");
+        timer.delay_ms(1_000_u32);
     }
 }
 
